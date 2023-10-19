00bf2f88485da831ace1c714fefffd0a33f2c938
Ernest <ernest20145@gmail.com>
2023-10-19 18:16:41 +0300

    added trainer photo

--- src/lib/trainersData.jsx
+++ src/lib/trainersData.jsx
@@ -1,9 +1,6 @@
-import trainer_one from "../assets/images/trainers/1.jpg";
-import trainer_two from "../assets/images/trainers/2.jpg";
-import trainer_three from "../assets/images/trainers/3.jpg";
-import trainer_four from "../assets/images/trainers/4.jpg";
-import trainer_five from "../assets/images/trainers/5.jpg";
-import trainer_six from "../assets/images/trainers/6.jpg";
+import trainer_one from "../assets/images/trainers/1.jpeg";
+import trainer_three from "../assets/images/trainers/3.jpeg";
+import trainer_six from "../assets/images/trainers/6.jpeg";
 export const trainerData = [
   [
     {
@@ -32,7 +29,7 @@ export const trainerData = [
   [
     {
       id: 1,
-      img: trainer_two,
+      img: trainer_three,
       name: "John Swift ",
       experience: "Trainer. Experience of 7 years",
       specialization: [
